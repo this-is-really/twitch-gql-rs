bec15b11b6cde04708b26298b9b23c3533e52ad1
this-is-really <grootar789@outlook.com>
2025-10-13 14:32:17 +0000

    Created an enum with errors.

---
+++ src/error.rs
@@ -0,0 +1,26 @@
+use thiserror::Error;
+#[derive(Error, Debug)]
+pub enum SystemError {
+    #[error("The file already exists")]
+    FileAlredyExists,
+    #[error("An error occurred during serialization: {0}")]
+    SerializationProblem(serde_json::Error),
+    #[error("An error occurred during deserialization: {0}")]
+    DeserializationProblem(serde_json::Error),
+    #[error("File not found")]
+    FileNotFound,
+    #[error("Failed to retrieve the required headers: {0}")]
+    HeadersError(#[from] Box<dyn std::error::Error>),
+    #[error("Error creating client: {0}")]
+    ClientBuilderError(#[from] reqwest::Error),
+}
+
+#[derive(Debug, Error)]
+pub enum TwitchError {
+    #[error("Twitch response JSON is missing the field: {0}")]
+    MissingField(String),
+    #[error("HTTP error: {0}")]
+    HttpError(u16),
+    #[error("Failed reqwest Twitch response: {0}")]
+    ReqwestProblem(#[from] reqwest::Error),
+}
